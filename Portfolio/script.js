eb9cf9d34ca5feff70eb68c5ab83e361183e1ae0
Chagallah <hchagalla@gmail.com>
2022-08-25 12:53:51 +0300

    initial commit

---
+++ Portfolio/script.js
@@ -0,0 +1,7 @@
+/* ====================== typing animation ===================== */
+var typed = new Typed("typing",{
+    strings:["Programmer","Web Developer","Front End Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    looop:true
+})
